9513eb261c49cc6081cc75f6138b5d7f159e7c5b
Luu Nguyen Bao Ngoc <baongoc11096@gmail.com>
2026-01-27 15:38:18 +0700

    add temp_humi_monitor code

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 
 #include "led_blinky.h"
 #include "neo_blinky.h"
+#include "temp_humi_monitor.h"
 
 void setup() {
   // put your setup code here, to run once:
@@ -10,6 +11,7 @@ void setup() {
 
   xTaskCreate(led_blinky, "Task LED Blink", 2048, NULL, 2, NULL);
   xTaskCreate(neo_blinky, "Task NEO Blink", 2048, NULL, 2, NULL);
+  xTaskCreate(temp_humi_monitor, "Task TEMP HUMI Monitor", 2048, NULL, 2, NULL);
 }
 
 void loop() {
