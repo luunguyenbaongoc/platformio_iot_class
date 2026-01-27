e2b1702258f69a6c3d7699125b19d8023cf4b6b1
Luu Nguyen Bao Ngoc <baongoc11096@gmail.com>
2026-01-27 09:53:56 +0700

    add TaskTemperature_Humidity

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include "DHT20.h"
 
 void TaskLEDControl(void *pvParameters) {
   pinMode(GPIO_NUM_48, OUTPUT); // Initialize LED pin
@@ -15,13 +16,28 @@ void TaskLEDControl(void *pvParameters) {
   }
 }
 
+void TaskTemperature_Humidity(void *pvParameters) {
+  // Placeholder for temperature and humidity sensor initialization
+  DHT20 dht20;
+  Wire.begin(GPIO_NUM_11, GPIO_NUM_12);
+  dht20.begin();
+  while(1) {
+    dht20.read();
+    double temperature = dht20.getTemperature();
+    double humidity = dht20.getHumidity();
+
+    Serial.printf("Temperature: %.2f C, Humidity: %.2f %%\n", temperature, humidity);
+    Serial.println();
+    vTaskDelay(5000);
+  }
+}
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
   xTaskCreate(TaskLEDControl, "LED Control", 2048, NULL, 2, NULL);
+  xTaskCreate(TaskTemperature_Humidity, "Temperature and Humidity", 2048, NULL, 2, NULL);
 }
 
 void loop() {
-  // Serial.println("Hello Custom Board");
-  // delay(1000);
 }
