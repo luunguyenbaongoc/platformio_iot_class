b32a04cf09081e0e3a2da6c55ca184935f37d5af
Luu Nguyen Bao Ngoc <baongoc11096@gmail.com>
2026-01-29 11:31:00 +0700

    add coreiot code

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include "temp_humi_monitor.h"
 #include "mainserver.h"
 #include "tinyml.h"
+#include "coreiot.h"
 
 void setup() {
   // put your setup code here, to run once:
@@ -14,8 +15,9 @@ void setup() {
   xTaskCreate(led_blinky, "Task LED Blink", 2048, NULL, 2, NULL);
   xTaskCreate(neo_blinky, "Task NEO Blink", 2048, NULL, 2, NULL);
   xTaskCreate(temp_humi_monitor, "Task TEMP HUMI Monitor", 2048, NULL, 2, NULL);
-  xTaskCreate(main_server_task, "Task Main Server" ,8192  ,NULL  ,2 , NULL);
-  xTaskCreate(tiny_ml_task, "Tiny ML Task" ,2048  ,NULL  ,2 , NULL);
+  // xTaskCreate(main_server_task, "Task Main Server" ,8192  ,NULL  ,2 , NULL);
+  // xTaskCreate(tiny_ml_task, "Tiny ML Task" ,2048  ,NULL  ,2 , NULL);
+  xTaskCreate(coreiot_task, "CoreIOT Task" ,4096  ,NULL  ,2 , NULL);
 }
 
 void loop() {
