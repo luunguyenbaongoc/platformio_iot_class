a2f92a8631c4ffbe3874f6848d67a4954161f0ec
Luu Nguyen Bao Ngoc <baongoc11096@gmail.com>
2026-01-29 09:20:04 +0700

    add tinyml code

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include "neo_blinky.h"
 #include "temp_humi_monitor.h"
 #include "mainserver.h"
+#include "tinyml.h"
 
 void setup() {
   // put your setup code here, to run once:
@@ -14,6 +15,7 @@ void setup() {
   xTaskCreate(neo_blinky, "Task NEO Blink", 2048, NULL, 2, NULL);
   xTaskCreate(temp_humi_monitor, "Task TEMP HUMI Monitor", 2048, NULL, 2, NULL);
   xTaskCreate(main_server_task, "Task Main Server" ,8192  ,NULL  ,2 , NULL);
+  xTaskCreate(tiny_ml_task, "Tiny ML Task" ,2048  ,NULL  ,2 , NULL);
 }
 
 void loop() {
